ae4a50a9dfe33122f1dce2a8a767837b060cd9c9
Roman Yarkov <roman121998yarkov@gmail.com>
2024-06-07 12:30:59 +0400

    Done

--- Factory2/Shape_Factory.cpp
+++ Factory2/Shape_Factory.cpp
@@ -21,25 +21,8 @@ Shape_Factory::Shape_Factory()
 	}
 }
 
-Shape_Factory::Shape_Factory(unsigned int x, unsigned int y, unsigned int live_width, MyGeometry::Color color)
+Shape_Factory::Shape_Factory(unsigned int x, unsigned int y, unsigned int live_width, MyGeometry::Color color):Shape_Factory()
 {	
-	int r = rand() % 4;
-	if (r == 0)
-	{
-		this->shape = new MyGeometry::Rectangle;
-	}
-	else if (r == 1)
-	{
-		this->shape = new MyGeometry::Circle;
-	}
-	else if (r == 2)
-	{
-		this->shape = new MyGeometry::Square;
-	}
-	else if (r == 3)
-	{
-		this->shape = new MyGeometry::Triangle;
-	}
 	this->shape->set_x(x);
 	this->shape->set_y(y);
 	this->shape->set_line_width(live_width);
